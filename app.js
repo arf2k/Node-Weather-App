776dea70fc1ef54c6eec8436a981456ada023378
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-05-26 16:14:41 -0400

    String interpolation example response

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const request = require("postman-request")
 
-const url = "http://api.weatherstack.com/current?access_key=847a77807b906c449a1154dfcfe6645e&query=37.83,-122.42"
+const url = "http://api.weatherstack.com/current?access_key=847a77807b906c449a1154dfcfe6645e&query=37.83,-122.42&units=f"
 
 request({ url: url, json: true }, (error, response) => {
-     console.log(response.body.current)
+     let temp = response.body.current.temperature
+     let feels = response.body.current.feelslike
+     console.log(` It is currently ${temp} degrees and feels like ${feels} degrees.` )
 })
+
