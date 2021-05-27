0754426bc9b7f9e9652922a2c9883005169afaf4
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-05-27 16:34:59 -0400

    Add mapbox api geocoding search functionality and test

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const request = require("postman-request")
+const request = require("postman-request");
 
-const url = "http://api.weatherstack.com/current?access_key=847a77807b906c449a1154dfcfe6645e&query=37.83,-122.42&units=f"
+const url =
+  "http://api.weatherstack.com/current?access_key=847a77807b906c449a1154dfcfe6645e&query=37.83,-122.42&units=f";
 
 request({ url: url, json: true }, (error, response) => {
-     let temp = response.body.current.temperature
-     let feels = response.body.current.feelslike
-     console.log(` It is currently ${temp} degrees and feels like ${feels} degrees.` )
-})
+  let temp = response.body.current.temperature;
+  let feels = response.body.current.feelslike;
+  let descrip = response.body.current.weather_descriptions[0].toLowerCase();
+//   console.log(
+//     ` It is currently ${temp} degrees and feels like ${feels} degrees and ${descrip}`
+//   );
+});
 
+
+// Geocoding
+//user provides address > convert to lat/long > weather 
+
+let geocodingUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZm9yZW1hbjYxMiIsImEiOiJja3A1d3l5bGwwMmVzMndxZTU3NTNyZXloIn0.NdY_Vj5xN5J09CBqfWNJzA"
+
+request({ url: geocodingUrl, json: true}, (error, response) => {
+     let long = response.body.features[0].center[0]
+     let lat = response.body.features[0].center[1]
+     console.log(`The coordinates for Los Angeles are ${lat} latitude and ${long} longitude`)
+})
